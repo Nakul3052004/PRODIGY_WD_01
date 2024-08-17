2aaa4d4a7bf61c301e5187649eb0d23e57206ba2
Nakul3052004 <nk30052004@gmail.com>
2024-08-17 15:58:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to change the background color of the navbar on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
